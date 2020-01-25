cda02cb354056dbad22811648be7220b06d79dd3
donnywarren <51206190+donnywarren@users.noreply.github.com>
2020-01-25 17:55:28 -0500

    logic is working

--- index.js
+++ index.js
@@ -1,39 +1,62 @@
 document.addEventListener('DOMContentLoaded', function() {
 
 
-  // Attach a “submit” event listener to the form and grab the value of the input. Make
-  // sure to convert the value of the input to a number with the below method
-  // a. parseInt(value)
-
   var maxNumber = 10;
-  var randomNumber = Math.ceil(Math.random()*maxNumber);
+  var randomNumber;
+  var number;
 
-  document.querySelector("input").addEventListener("submit", function(event){
+  function randomNumbers(){
+    randomNumber = Math.ceil(Math.random()*maxNumber);
+  }
+    randomNumbers();
+    console.log(`outside  ${randomNumber}`);
+
+
+
+  document.getElementById("guess").addEventListener("submit", function(event){
     event.preventDefault();
-    var guess = document.querySelector("input").value;
-    console.log(`${guess} is the input guess.`);
-    checkStatus(guess);
-  });
+    var userInput = document.getElementById("input").value;
+    var number = parseInt(userInput);
 
+    document.getElementById("input").value="";
 
-  console.log(`Random number ${randomNumber}`);
+    console.log(`This is the parsed numb ${number}`);
 
-  checkStatus(guessNumber);
-
-  function checkStatus(number) {
-    if(isNaN(number)){
-      console.log("You must enter a number.");
-    } else if(number < 1 || number > 10){
-      console.log("You must guess a number between 1 and 10.");
-    } else if(number > randomNumber){
-      console.log(`The number you seek is less than ${number}`);
-    } else if(number < randomNumber){
-      console.log(`The number you seek is greater than ${number}`);
-    } else if(number === randomNumber){
-      console.log(`You win! ${number} is the right nubmer!`)
+    function checkStatus(number) {
+      if(isNaN(number)){
+        console.log("You must enter a number.");
+      } else if(number < 1 || number > 10){
+        console.log("You must guess a number between 1 and 10.");
+      } else if(number > randomNumber){
+        console.log(`The number you seek is less than ${number}`);
+      } else if(number < randomNumber){
+        console.log(`The number you seek is greater than ${number}`);
+      } else if(number === randomNumber){
+        console.log(`You win! ${number} is the right nubmer!`)
+      }
     }
 
-  }
+    checkStatus(number);
+
+});
+
+
+
+
+
+
+  //
+  // document.querySelector("input").addEventListener("submit", function(event){
+  //   event.preventDefault();
+  //   var guess = document.querySelector("input").value;
+  //   console.log(`${guess} is the input guess.`);
+  //   checkStatus(guess);
+  // });
+  //
+  //
+  // console.log(`Random number ${randomNumber}`);
+  //
+  // checkStatus(guessNumber);
 
 
 
